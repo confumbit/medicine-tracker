b6130c922ae3d2c6861dfcd7a6ceb6b626a05fff
Heatblast <heatblast0044@gmail.com>
2022-06-02 22:51:30 +0530

    Add whatsapp link, color change of tile for low quanitity

--- public/script.js
+++ public/script.js
@@ -4,10 +4,24 @@ fetch("/medicines")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    order = "";
     data.forEach((medicine) => {
       let block = document.createElement("div");
       block.classList.add("block");
       block.innerText = medicine.name;
+
+      if (medicine.quantity <= medicine.min) {
+        block.style.backgroundColor = "rgb(201, 163, 163)";
+        let arr = medicine.name.split(/(\s+)/);
+        arr.forEach((i) => {
+          if (i == " ") {
+            order += "%20";
+          } else {
+            order += i;
+          }
+        });
+        order += "%0A";
+      }
 
       let quan = document.createElement("span");
       quan.classList.add("quan");
@@ -15,5 +29,9 @@ fetch("/medicines")
 
       block.appendChild(quan);
       container.appendChild(block);
+
+      console.log(order);
+      let wa = document.querySelector(".walink");
+      wa.href = "https://wa.me/919440089794?text=" + order;
     });
   });
